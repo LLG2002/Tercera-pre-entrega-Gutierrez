d09031e98ec64fdd368d7e2a1d0594e6a3cdd91f
LLG2002 <lucasgutierrez2010@hotmail.com>
2023-04-25 20:22:27 -0300

    cbv2

--- inicio/forms.py
+++ inicio/forms.py
@@ -1,19 +1,14 @@
 from django import forms
 
 class CreacionClienteFormulario(forms.Form):
-    usuario = forms.CharField(max_length=15)
-    contraseña = forms.CharField(max_length=15)
-    email= forms.CharField(max_length=25)
-    telefono= forms.IntegerField()
-
-class BuscarCliente(forms.Form):
     usuario = forms.CharField(max_length=15, required=False)
     contraseña = forms.CharField(max_length=15, required=False)
     email= forms.CharField(max_length=30, required=False)
     telefono= forms.IntegerField(required=False)
+
+class BuscarCliente(CreacionClienteFormulario):
+    ...
     
-class EditarUsuarioFormulario(forms.Form):
     
-    contraseña = forms.CharField(max_length=15, required=False)
-    email= forms.CharField(max_length=30, required=False)
-    telefono= forms.IntegerField(required=False)
+class EditarUsuarioFormulario(CreacionClienteFormulario):
+    ...
--- inicio/views.py
+++ inicio/views.py
@@ -4,46 +4,53 @@ from inicio.forms import CreacionClienteFormulario, BuscarCliente, EditarUsuario
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
+from django import forms
+
+class BaseClientes(forms.Form):
+    model = Clientes
+    forms = CreacionClienteFormulario
+    fields = ['usuario', 'contraseña', 'email', 'telefono']
 
 class ListaClientes(ListView):
     model = Clientes
     template_name = 'inicio/lista_clientes_cbv.html'
-    forms = BuscarCliente
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["usuario."] = self.get
-        return context
     
 class CrearUsuario(CreateView):
     model = Clientes
+    forms = CreacionClienteFormulario
+    fields = ['usuario', 'contraseña', 'email', 'telefono']
     template_name ='inicio/crear_usuario_cbv.html'
     success_url = '/clientes/'
-    fields = ['usuario', 'contraseña', 'email', 'telefono']
-    forms = CreacionClienteFormulario
+    
     
 class EditarUsuario(UpdateView):
     model = Clientes
-    template_name ='inicio/editar_cbv.html'
-    success_url = 'inicio/lista_clientes_cbv.html'
+    # forms = EditarUsuarioFormulario
     fields = ['usuario', 'contraseña', 'email', 'telefono']
-    forms = EditarUsuarioFormulario
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["usuario"] = self.post
-        return context
-
-class EliminarUsuario(DeleteView):
-    model = Clientes
-    template_name = 'inicio/eliminar_usuario_cbv.html'
-    succes_url = 'inicio/lista_clientes_cbv.html'
+    template_name ='inicio/editar_cbv.html'
+    success_url = '/clientes/'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["usuario"] = self.object
         return context
 
-class MostrarUsuario(DetailView):
+class EliminarUsuario(BaseClientes,DeleteView):
+    model = Clientes
+    template_name = 'inicio/eliminar_usuario_cbv.html'
+    succes_url = '/clientes/'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["usuario"] = self.object
+        return context
+
+class MostrarUsuario(BaseClientes,DetailView):
     model = Clientes
     template_name = 'inicio/mostrar_usuario_cbv.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["usuario"] = self.object
+        return context
+
